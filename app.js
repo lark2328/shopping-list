2dec0e22ebff4a4339990a5b88702ffae382eb2e
lark2328 <shinra_girl23@yahoo.com>
2014-04-21 20:08:12 -0400

    remove and highlight added

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ $('button').on('click', function() {
 	//alert('click button');
 	value = $("#box").val();
 	//alert(value);
-	$('#list').append(value);
+	//$('#list').append(value);
+	$('li').first().next().append(value);
+});
+
+$('li').dblclick(function() {
+	$(this).remove();
+});
+
+var classHighlight = 'highlight';
+var $lis = $('li').click(function(e) {
+	e.preventDefault();
+	$lis.removeClass(classHighlight);
+	$(this).addClass(classHighlight);
 
 
 });
 
 });
+
+//$('li').click(function() {
+//	$(this).effect('highlight', {color:"#669966"}, 3000);
